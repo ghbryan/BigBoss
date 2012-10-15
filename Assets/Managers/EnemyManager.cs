7c320d5b1ae573fc550b3ceb7f16696e674abac1
ghbryan <ghbryan@gmail.com>
2012-10-15 01:25:25 -0700

    Added LevelManager and Fixed Enemy Spawning
    The LevelManager will hold all of the various data for any given level
    (enemy types, number of waves, etc.) This also circumvents an issue
    where Unity cannot locate inactive objects.

---
+++ Assets/Managers/EnemyManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class EnemyManager : MonoBehaviour {
+	
+	private GameObject[] spawners;
+	private GameObject[] enemyTypes;
+	
+	public float minTimer = 2.0f;
+	public float maxTimer = 4.0f;
+	
+	private float spawnDelay;
+	
+	// Use this for initialization
+	void Start () {
+		spawners = GameObject.FindGameObjectsWithTag("EnemySpawner");
+		enemyTypes = GameObject.Find ("LevelManager").GetComponent<LevelManager>().enemyData;
+		spawnDelay = minTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spawnDelay -= Time.deltaTime;
+		
+		if(spawnDelay <= 0) {
+			Vector3 spawnPos = spawners[Random.Range (0, spawners.Length)].transform.position;
+			
+			if(enemyTypes.Length > 0)
+			{
+				GameObject nextEnemyType = enemyTypes[Random.Range (0, enemyTypes.Length)];
+				GameObject newEnemy = (GameObject)Instantiate (nextEnemyType, spawnPos, Quaternion.identity);
+				//newEnemy.AddComponent<Enemy>();
+			}
+			else
+			{
+				Debug.Log("EnemyManager:Update(): No enemyTypes found!");;
+			}
+			
+			spawnDelay = Random.Range (minTimer, maxTimer);
+		}
+	}
+}
